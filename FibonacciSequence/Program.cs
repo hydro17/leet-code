5f8478d2c06538bdf01996f1d74bb0831f7d9287
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2020-01-03 20:10:13 +0100

    Add project files.

---
+++ FibonacciSequence/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Numerics;
+
+namespace FibonacciSequence
+{
+    class Program
+    {
+        private static int sum = 0; 
+        private static BigInteger?[] memo;
+
+        static void Main(string[] args)
+        {
+            //for(int i = 0; i <=7; i++)
+            //{
+            //    Console.WriteLine($"fibRec({i}) = {FibRecursive(i)}");
+            //}
+
+            //for (int i = 0; i <= 7; i++)
+            //{
+            //    Console.WriteLine($"fibImp({i}) = {FibImperative(i)}");
+            //}
+
+            BigInteger n = 40;
+
+            sum = 0;
+            memo = new BigInteger?[(long)n+1];
+            memo[0] = BigInteger.Zero;
+            memo[1] = BigInteger.One;
+
+            Console.WriteLine("long MaxValue = ");
+            Console.WriteLine("{0:n0}", long.MaxValue);
+            Console.WriteLine("sum = " + sum);
+            Console.WriteLine(n);
+            Console.WriteLine("{0:n}", FibRecursive(n));
+            Console.WriteLine("sum = {0:n0}", sum);
+        }
+
+        private static BigInteger FibRecursive(BigInteger n)
+        {
+            sum += 1;
+            if (n == BigInteger.Zero) return BigInteger.Zero;
+            if (n == BigInteger.One) return BigInteger.One;
+            if (memo[(long)n] != null) return (BigInteger)memo[(long)n];
+
+            memo[(long)n] = FibRecursive(n - 1) + FibRecursive(n - 2);
+
+            //return FibRecursive(n - 1) + FibRecursive(n - 2);
+
+            return (BigInteger)memo[(long)n];
+        }
+
+        //private static long FibImperative(long n)
+        //{
+        //    if (n == 0) return 0;
+        //    if (n == 1) return 1;
+
+        //    int x = 0, y = 1, z = 0;
+
+        //    for (int i = 2; i <= n; i++)
+        //    {
+        //        z = x + y;
+        //        x = y;
+        //        y = z;
+        //    }
+
+        //    return z;
+        //}
+
+        //private static long FibMemoization(long n)
+        //{
+        //    if (n == 0) return 0;
+        //    if (n == 1) return 1;
+
+        //    return FibRecursive(n - 1) + FibRecursive(n - 2);
+        //}
+    }
+}
---
+++ LongestPalindromicSubstring/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace LongestPalindromicSubstring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string possiblePalindrome = "abba";
+
+            Solution s = new Solution();
+
+            //Console.WriteLine($"abba: {s.IsPalindrome("abba")}");
+            //Console.WriteLine($"abb: {s.IsPalindrome("abb")}");
+            //Console.WriteLine($"b: {s.IsPalindrome("b")}");
+        }
+
+       
+
+        //public static string LongestPalindrome(string s)
+        //{
+
+        //}
+    }
+
+    public class Solution
+    {
+        public string LongestPalindrome(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+
+            }
+            str.Substring()
+
+            return null;
+        }
+
+        private bool IsPalindrome(string possiblePalindrome)
+        {
+            for (int i = 0, j = possiblePalindrome.Length - 1; i < j; i++, j--)
+            {
+                if (possiblePalindrome[i] != possiblePalindrome[j]) return false;
+            }
+
+            return true;
+        }
+    }
+}
