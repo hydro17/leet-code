0f9b73c66b1b50cc26110336df2032a9496965a4
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2020-01-12 18:42:58 +0100

    working solution

---
+++ 3Sum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           IList<IList<int>> subsets = new Solution().ThreeSum(new[] { 1, 2, 0, -2, 3 , 4, -1});
+
+            foreach (List<int> intList in subsets)
+            {
+                foreach (int i in intList)
+                {
+                    Console.Write($"{i},");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 3Sum/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _3Sum
+{
+    public class Solution
+    {
+        private IList<IList<int>> _3itemSubsets = new List<IList<int>>();
+
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            Make3ItemSubsets(new List<int>(), nums);
+
+            return _3itemSubsets.Where(list => list.Sum(i => i) == 0).ToList();
+        }
+
+        private void Make3ItemSubsets(IList<int> subset, int[] nums)
+        {
+            if (subset.Count == 3)
+            {
+                _3itemSubsets.Add(subset);
+                return;
+            }
+
+            if (nums.Length == 0) return;
+
+            IList<int> newSubsetPlusNewItem = new List<int>(subset);
+            newSubsetPlusNewItem.Add(nums[0]);
+            
+            IList<int> newSubsetWithoutNewItem = new List<int>(subset);
+
+            int[] numsWithoutOneItem = nums.Where((val, idx) => idx > 0).ToArray();
+
+            Make3ItemSubsets(newSubsetPlusNewItem, numsWithoutOneItem);
+            Make3ItemSubsets(newSubsetWithoutNewItem, numsWithoutOneItem);
+        }
+    }
+
+}
