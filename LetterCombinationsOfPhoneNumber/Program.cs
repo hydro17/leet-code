2038b153889ab01769241cc8622cbda89e305255
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2020-01-13 20:57:26 +0100

    LetterCombinationsOfPhoneNumber - solution

---
+++ LetterCombinationsOfPhoneNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LetterCombinationsOfPhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<string> letterCombinations = new Solution().LetterCombinations("23");
+
+            foreach(string comb in letterCombinations)
+            {
+                Console.WriteLine(comb);
+            }
+        }
+    }
+}
---
+++ LetterCombinationsOfPhoneNumber/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LetterCombinationsOfPhoneNumber
+{
+    class Solution
+    {
+        private IDictionary<char, string> digitLetterMapping = new Dictionary<char, string>
+        {
+            ['2'] = "abc",
+            ['3'] = "def",
+            ['4'] = "ghi",
+            ['5'] = "jkl",
+            ['6'] = "mno",
+            ['7'] = "pqrs",
+            ['8'] = "tuv",
+            ['9'] = "wxyz"
+        };
+
+        private IList<string> letterCombinations = new List<string>();
+
+        public IList<string> LetterCombinations(string digits)
+        {
+            MakeLetterCombinations("", digits);
+
+            return letterCombinations;
+        }
+
+        private void MakeLetterCombinations(string letterCombination, string digits)
+        {
+            if (digits.Length == 0 && letterCombination.Length != 0)
+            {
+                letterCombinations.Add(letterCombination);
+                return;
+            }
+
+            string letters = digitLetterMapping[digits[0]];
+
+            for (int i = 0; i < letters.Length; i++)
+            {
+                MakeLetterCombinations(letterCombination + letters[i], digits.Substring(1));
+            }
+        }
+    }
+}
